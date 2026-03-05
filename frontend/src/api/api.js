3d27ca2b44106d18a045aefbb1fc76ba1e018a66
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2026-03-05 17:48:01 +0000

    correcting code

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://automatic-eureka-xjw4xxrpqf69jj-8000.app.github.dev"
 });
 
 /* PROMPTS */
